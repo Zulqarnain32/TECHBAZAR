c95282a1057841b57ce0fcd95aad6235ca278f44
Zulqarnain <zulqarnainchohan250@gmail.com>
2025-04-23 05:08:38 -0700

    atlas connection

--- server/dbConfig/dbConnect.js
+++ server/dbConfig/dbConnect.js
@@ -14,7 +14,8 @@ const products = require("../data/productData");
 const Product = require("../models/productModel");
 
 const dbConnect = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/TechBazar")
+    // mongoose.connect("mongodb://127.0.0.1:27017/TechBazar")
+    mongoose.connect("mongodb+srv://zulqarnainc67:8edFlYCAniXGuPQY@cluster0.rh2svnp.mongodb.net/")
         .then(async () => {
             console.log("Connected successfully to MongoDB");
 
