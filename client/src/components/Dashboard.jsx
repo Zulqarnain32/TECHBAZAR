2a7046a4bf00298ef18ef9930d464f2d85c2792c
Zulqarnain <zulqarnainchohan250@gmail.com>
2025-04-24 16:43:23 -0700

    added dashboard correct wat

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -6,55 +6,53 @@ import { ImBin2 } from "react-icons/im";
 const Dashboard = () => {
   const [data, setData] = useState([]);
 
-  // useEffect(() => {
-  //   axios
-  //     .get("http://localhost:5000/dashboard")
-  //     .then((result) => {
-  //       console.log(result.data);
-  //       setData(result.data);
-  //     })
-  //     .catch((error) => console.error("Error fetching data:", error));
-  // }, []);
   useEffect(() => {
     axios
       .get("http://localhost:5000/dashboard")
       .then((result) => {
+        console.log(result.data);
         const filteredUsers = result.data.filter(
-          (user) => user.cart && user.cart.length > 0
+          (user) => user.userOrders[0]?.cart?.length > 0
         );
         setData(filteredUsers);
       })
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
-  
-
 
   const sendEmail = (email) => {
-    console.log("email ",email)
-    axios.post("http://localhost:5000/api/admin/email",{email})
-    .then((res) => {
-      console.log("email sent",res)
-    }).catch(err => {
-      console.log("error sending email")
-    })
-  
-  }
+    console.log("email ", email);
+    axios
+      .post("http://localhost:5000/api/admin/email", { email })
+      .then((res) => {
+        console.log("email sent", res);
+      })
+      .catch((err) => {
+        console.log("error sending email");
+      });
+  };
 
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <h1 className="text-3xl font-bold mb-6">Dashboard Page</h1>
       {data.map((user, index) => (
         <div key={index} className="mb-12 bg-white p-6 rounded-2xl shadow-md">
-          <div className="flex space-x-5 mb-5">
+          <div className="flex  mb-5 ">
+            <div className="flex space-x-5">
+              <h2 className="text-xl font-semibold text-gray-700 ">
+                {user.email}
+              </h2>
 
-          {/* <h2 className="text-xl font-semibold text-gray-700 mb-4">{user.username || user.displayName} - {user.email}</h2> */}
-          <h2 className="text-xl font-semibold text-gray-700 ">
-            {user.email}
-          </h2>
-          <button onClick={() => sendEmail(user.email)} className="bg-blue-400 px-4 py-2 text-white rounded-md ">Send Email</button>
+              <button
+                onClick={() => sendEmail(user.email)}
+                className="bg-blue-400 px-4 py-2 text-white rounded-md "
+              >
+                Send Email
+              </button>
+            </div>
+            <div className="text-2xl absolute right-12 font-semibold">Total: Rs. {user.userOrders[0]?.totalPrice}</div>
           </div>
 
-          {user.cart && user.cart.length > 0 ? (
+          {user.userOrders[0]?.cart?.length > 0 ? (
             <div className="overflow-x-auto">
               <table className="min-w-full divide-y divide-gray-200">
                 <thead className="bg-gray-200">
@@ -71,9 +69,7 @@ const Dashboard = () => {
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
                       Price
                     </th>
-                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
-                      Old Price
-                    </th>
+
                     <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">
                       Quantity
                     </th>
@@ -89,12 +85,12 @@ const Dashboard = () => {
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
-                  {user.cart.map((item, idx) => (
+                  {user.userOrders[0]?.cart?.map((item, idx) => (
                     <tr
                       key={idx}
                       className="hover:bg-gray-100 transition duration-150"
                     >
-                       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
                         <img src={item.image} className="w-[40px]" />
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
@@ -106,9 +102,7 @@ const Dashboard = () => {
                       <td className="px-6 py-4 whitespace-nowrap text-sm text-green-600 font-semibold">
                         Rs. {item.price}
                       </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 line-through">
-                        {item.oldPrice ? `Rs. ${item.oldPrice}` : "-"}
-                      </td>
+
                       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
                         {item.quantity}
                       </td>
@@ -120,9 +114,8 @@ const Dashboard = () => {
                         {item.stock}
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800">
-                        <ImBin2 className = "text-red-400 cursor-pointer"/>
+                        <ImBin2 className="text-red-400 cursor-pointer" />
                       </td>
-                     
                     </tr>
                   ))}
                 </tbody>
